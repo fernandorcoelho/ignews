3161cf4e8b93472289618fee71cb04c0924f74b0
Team <team@fastcode.com.br>
2022-01-29 18:42:59 -0300

    Home Page & SubscribeButton Component

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -3,7 +3,7 @@ import { FiX } from 'react-icons/fi';
 
 import styles from './styles.module.scss';
 
-export function SignInButton () {
+export function SignInButton() {
   const isUserLoggedIn = true;
 
   return isUserLoggedIn ? (
